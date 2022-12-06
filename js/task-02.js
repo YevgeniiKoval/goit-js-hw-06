c210c2c69f8209658cb0da2ac1538d246491eb3d
YevgeniiKoval <109030577+YevgeniiKoval@users.noreply.github.com>
2022-12-06 19:45:24 +0200

    HW

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,15 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
-];
+]
+const list = document.querySelector('#ingredients')
+const liArray = []
+
+ingredients.forEach(ingredient => {
+	const item = document.createElement('li')
+	item.className = 'item'
+	item.textContent = ingredient
+	liArray.push(item)
+})
+
+list.append(...liArray)
